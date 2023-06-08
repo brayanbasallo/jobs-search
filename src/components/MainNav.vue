3662080ef5a470470651e562817cad0fe108e36c
Brayan Basallo Soto <basallo17@gmail.com>
2023-06-08 17:40:52 -0500

    Main nav

---
+++ src/components/MainNav.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="w-full text-sm">
+        <div class="fixed top-0 left-0 w-full h-16 bg-white">
+            <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
+                <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+                <h2>
+                    Developed by {{ author.firstName }} {{ author.lastName }}
+                </h2>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'MainNav',
+    data() {
+        return {
+            company: "Bobo Careers",
+            author: {
+                firstName: "Brayan",
+                lastName: "Basallo Soto",
+            }
+        }
+    },
+}
+</script>
