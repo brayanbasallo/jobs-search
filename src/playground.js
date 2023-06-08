8521b8b2c796557cf83d3fed862a031a25e5439e
Brayan Basallo Soto <basallo17@gmail.com>
2023-06-08 18:09:17 -0500

    Test a vanilla function

---
+++ src/playground.js
@@ -0,0 +1,8 @@
+export const eventOrOdd = (num) => {
+    if (num % 2 === 0) {
+        return 'even';
+    }
+    return 'odd';
+}
+
+
--- tests/unit/playground.test.js
+++ tests/unit/playground.test.js
@@ -1,8 +1,21 @@
 import { describe, it, expect } from "vitest";
-
+import { eventOrOdd } from "@/playground";
 
 describe("basic math", () => {
     it("add two numbers", () => {
         expect(1 + 1).toBe(2);
+    });
+
+    describe("eventOrOdd", () => {
+        describe("when the number is even", () => {
+            it("indicates the number is even", () => {
+                expect(eventOrOdd(4)).toBe("even");
+            });
+        });
+        describe("when the number is odd", () => {
+            it("indicates the number is odd", () => {
+                expect(eventOrOdd(3)).toBe("odd");
+            });
+        });
     });
 });
