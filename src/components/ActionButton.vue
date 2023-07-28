76f9d385c7911f2d7f9165ed2590e66072d66473
Brayan Basallo Soto <basallo17@gmail.com>
2023-07-28 11:47:28 -0500

    add props for dinamic button class

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,12 +1,44 @@
 <template>
   <button
-    class="rounded border-0 px-5 py-3 font-medium text-white bg-brand-blue-1 hover:shadow-blue"
+    :class="buttonClass"  
   >
-    Sing in
+    {{ text }}
   </button>
 </template>
 <script>
 export default {
-  name: 'ActionButton'
+  name: 'ActionButton',
+  props: {
+    text: {
+      type: String,
+      default: 'Sign in',
+    },
+    type: {
+      type: String,
+      default: 'primary',
+    },
+  },
+  computed: {
+    buttonClass() {
+      return {
+        primary: this.type === 'primary',
+        secondary: this.type === 'secondary',
+      }
+    }
+  },
 }
 </script>
+
+<style scoped>
+button {
+  @apply rounded px-5 py-3 font-medium;
+}
+
+.primary {
+  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+}
+
+.secondary {
+  @apply bg-transparent text-brand-blue-1 hover:bg-brand-blue-2 hover:text-white;
+}
+</style>
