bf28a5a9bd7da80652a144560995642c26ca8c4e
Brayan Basallo Soto <basallo17@gmail.com>
2023-08-03 16:23:44 -0500

    add theHero

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full text-sm">
+  <header class="w-full text-sm" :class="headerHeightClass">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="mx-auto flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
@@ -26,6 +26,7 @@
 import ActionButton from '@/components/ActionButton.vue'
 import ProfileImage from '@/components/ProfileImage.vue'
 import TheSubnav from '@/components/TheSubnav.vue'
+
 export default {
   name: 'MainNav',
   components: {
@@ -39,6 +40,11 @@ export default {
       url: 'https://careers.google.com',
       menuItems: ['Teams', 'Location', 'Life at Bobo careers', 'How we hire', 'Students', 'Jobs'],
       isLoggedIn: false
+    }
+  },
+  computed: {
+    headerHeightClass() {
+      return this.isLoggedIn ? 'h-32' : 'h-16'
     }
   },
   methods: {
