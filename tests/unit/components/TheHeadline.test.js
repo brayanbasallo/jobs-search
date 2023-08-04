e34c498126a72f3a630c798656ac6788701ca76f
Brayan Basallo Soto <basallo17@gmail.com>
2023-08-04 14:48:27 -0500

    optimize the theheadline test

--- tests/unit/components/TheHeadline.test.js
+++ tests/unit/components/TheHeadline.test.js
@@ -11,29 +11,33 @@ describe('TheHeadline', () => {
     //         expect(mockFunction).toHaveBeenCalledWith(1, 2, 3);
     //     })
     // })
+    beforeEach(() => {
+        vi.useFakeTimers(); // use fake timers
+    })
+
+    afterEach(() => {
+        vi.useRealTimers(); // use real timers
+    })
+
+
     it('displays introduction action verb', () => {
-        vi.useFakeTimers();
         render(TheHeadline)
         const actionPhrase = screen.getByRole('heading', { 
             name: /build for everyone/i
         })
         expect(actionPhrase).toBeInTheDocument()
-        vi.useRealTimers();
     })
 
     it('change action verb at a consistent interval', () => {
-        vi.useFakeTimers(); // to mock the js timer functions
         const mock = vi.fn(); // mock function
         vi.stubGlobal('setInterval', mock); // stub the global setInterval function
 
         render(TheHeadline)
 
         expect(mock).toHaveBeenCalledTimes(1); // check if setInterval has been called once
-        vi.useRealTimers(); // restore the js timer functions
     })
 
     it('swaps action verb after interval', async () => {
-        vi.useFakeTimers();
         render(TheHeadline)
         vi.advanceTimersToNextTimer(); // advance the timer to the next timer
         await nextTick(); // wait for the next tick
@@ -41,18 +45,14 @@ describe('TheHeadline', () => {
             name: /create for everyone/i
         })
         expect(actionPhrase).toBeInTheDocument()
-        vi.useRealTimers();
     })
     
     it('removes interval when component is unmounted', () => {
-        vi.useFakeTimers();
-
         const clearInterval = vi.fn();
         vi.stubGlobal('clearInterval', clearInterval);
         const { unmount } = render(TheHeadline)
         unmount();
         expect(clearInterval).toHaveBeenCalledTimes(1);
         vi.unstubAllGlobals();
-        vi.useRealTimers();
     })
 })
