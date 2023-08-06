5d434356f33470c94672715240c594335fec77d8
Brayan Basallo Soto <basallo17@gmail.com>
2023-08-05 22:46:11 -0500

    add search form

---
+++ src/components/JobSearchForm.vue
@@ -0,0 +1,48 @@
+<template>
+    <form class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-gray-3">
+        <font-awesome-icon class="ml-4 mr-3" :icon="['fas', 'search']" />
+        <div class="flex flex-1 flex-nowrap h-full text-base font-light">
+            <div class="flex h-full flex-1 relative items-center pr-3">
+                <label for="role" class="absolute left-0 -top-10">Role</label>
+                <input 
+                    id="role" 
+                    v-model="role"
+                    type="text" 
+                    name="role" 
+                    placeholder="Sofware engineer" 
+                    class="w-full font-normal focus:outline-none" 
+                />
+            </div>
+            <span class="flex items-center h-full border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3">
+                in
+            </span>
+            <div class="flex h-full flex-1 relative items-center pl-3">
+                <label for="location" class="absolute left-0 -top-10">where?</label>
+                <input 
+                    id="location"
+                    v-model="location"
+                    type="text" 
+                    name="location" 
+                    placeholder="Los angeles" 
+                    class="w-full font-normal focus:outline-none" 
+                />
+            </div>
+        </div>
+        <ActionButton text="Search" type="secondary" class="rounded-r-3xl" />
+    </form>
+</template>
+<script>
+import ActionButton from '@/components/ActionButton.vue';
+export default {
+    name: 'JobSearchForm',
+    components: {
+        ActionButton
+    },
+    data() {
+        return {
+            role: '',
+            location: ''
+        }
+    },
+}
+</script>
--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,18 +1,20 @@
 <template>
     <main>
-        <section class="flex flex-col h-screen bg-yellow-200 pt-10 pb-20">
+        <section class="flex flex-col h-screen pt-10 pb-20">
             <div class="grid grid-cols-12">
-                <div class="col-span-1 col-start-1 border border-blue-700">
-                    1 column
+                <div class="col-span-1 col-start-1">
                 </div>
-                <div class="col-span-5 col-start-2 border border-blue-700">
+                <div class="col-span-5 col-start-2">
                 <the-headline />
+                <job-search-form />
                 </div>
-                <div class="col-span-5 col-start-7 border border-blue-700">
-                    5 column
+                <div class="col-span-5 col-start-7 self-center justify-self-center">
+                    <img 
+                    class="w-80 h-80 object-contain"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png" 
+                    alt="">
                 </div>
-                <div class="col-span-1 col-start-12 border border-blue-700">
-                    1 column
+                <div class="col-span-1 col-start-12">
                 </div>
             </div>
         </section>
@@ -20,11 +22,13 @@
 </template>
 <script>
 import TheHeadline from '@/components/TheHeadline.vue';
+import JobSearchForm from '@/components/JobSearchForm.vue';
 
 export default {
     name: 'TheHero',
     components: {
         TheHeadline,
+        JobSearchForm,
     },
 }
 </script>
