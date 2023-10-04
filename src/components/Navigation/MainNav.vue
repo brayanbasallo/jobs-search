3dd7fbfb12f8e82c3d022565cb6f357f88c660ae
Brayan Basallo Soto <basallo17@gmail.com>
2023-10-03 22:22:11 -0500

    test: fix test using original stores
    important, we can mock pinia and is the sugestion

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -2,8 +2,8 @@
   <header class="w-full text-sm" :class="headerHeightClass">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="mx-auto flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
-        <router-link :to="{name:'Home'}" class="flex h-full items-center text-xl">
-          Bobo Careers  
+        <router-link :to="{ name: 'Home' }" class="flex h-full items-center text-xl">
+          Bobo Careers
         </router-link>
         <nav class="ml-12 h-full">
           <ul class="flex h-full">
@@ -15,11 +15,11 @@
           </ul>
         </nav>
         <div class="ml-auto flex h-full items-center">
-          <profile-image v-if="isLoggedIn" />
-          <action-button v-else text="sing in" type="primary" @click="logginUser"  />
+          <profile-image v-if="userStore.isLoggedIn" />
+          <action-button v-else text="sing in" type="primary" @click="userStore.loginUser" />
         </div>
       </div>
-      <the-subnav v-if="isLoggedIn" />
+      <the-subnav v-if="userStore.isLoggedIn" />
     </div>
   </header>
 </template>
@@ -28,6 +28,8 @@
 import ActionButton from '@/components/Shared/ActionButton.vue'
 import ProfileImage from '@/components/Navigation/ProfileImage.vue'
 import TheSubnav from '@/components/Navigation/TheSubnav.vue'
+import { mapStores } from 'pinia'
+import { useUserStore } from '@/stores/user'
 
 export default {
   name: 'MainNav',
@@ -39,24 +41,19 @@ export default {
   data() {
     return {
       menuItems: [
-        { text: 'Teams', url: '/'}, 
-        { text: 'Location', url: '/'}, 
-        { text: 'Life at Bobo careers', url: '/'}, 
-        { text: 'How we hire', url: '/'}, 
-        { text: 'Students', url: '/'}, 
-        { text: 'Jobs', url: '/jobs/results'}
-      ],
-      isLoggedIn: false
+        { text: 'Teams', url: '/' },
+        { text: 'Location', url: '/' },
+        { text: 'Life at Bobo careers', url: '/' },
+        { text: 'How we hire', url: '/' },
+        { text: 'Students', url: '/' },
+        { text: 'Jobs', url: '/jobs/results' }
+      ]
     }
   },
   computed: {
+    ...mapStores(useUserStore),
     headerHeightClass() {
-      return this.isLoggedIn ? 'h-32' : 'h-16'
-    }
-  },
-  methods: {
-    logginUser() {
-      this.isLoggedIn = true
+      return this.userStore.isLoggedIn ? 'h-32' : 'h-16'
     }
   }
 }
--- tests/unit/components/Navigation/MainNav.test.js
+++ tests/unit/components/Navigation/MainNav.test.js
@@ -1,65 +1,66 @@
-import {render, screen} from '@testing-library/vue'
+import { render, screen } from '@testing-library/vue'
 import userEvent from '@testing-library/user-event'
 import { RouterLinkStub } from '@vue/test-utils'
+import { createTestingPinia } from '@pinia/testing'
 
 import MainNav from '@/components/Navigation/MainNav.vue'
 
 describe('MainNav', () => {
-    const renderMainNav = () => {
-        const $route = {
-            name: "Home"
-        }
-        render(MainNav, {
-            global: {
-                stubs: {
-                    FontAwesomeIcon: true,
-                    RouterLink: RouterLinkStub,
-                },
-                mocks: {
-                    $route
-                }
-            },
-        })
+  const renderMainNav = () => {
+    const pinia = createTestingPinia({ stubActions: false })
+    const $route = {
+      name: 'Home'
     }
-
-    it('display company name', () => {
-        renderMainNav()
-        //screen.debug() // uncomment to see the rendered HTML
-        const companyName = screen.getByText('Bobo Careers')
-        expect(companyName).toBeInTheDocument()
+    render(MainNav, {
+      global: {
+        plugins: [pinia],
+        stubs: {
+          FontAwesomeIcon: true,
+          RouterLink: RouterLinkStub
+        },
+        mocks: {
+          $route
+        }
+      }
     })
-    it("displays menu items fot navegations", () => {
-        renderMainNav()
-        const navegationMenuItems = screen.getAllByRole("listitem")
-        const navegationMenuItemsText = navegationMenuItems.map(
-            (item) => item.textContent
-        );
-        expect(navegationMenuItemsText).toEqual([
-            "Teams",
-            "Location",
-            "Life at Bobo careers",
-            "How we hire",
-            "Students",
-            "Jobs"
-        ])        
-    })
+  }
 
-    describe("When the user logs in", () => {
-        it("displays the profile picture", async () => {
-            renderMainNav()
-            let profileImage = screen.queryByRole("img", {
-                name: /user profile image/i, // use a regex to match the alt text
-            })
-            expect(profileImage).not.toBeInTheDocument()
+  it('display company name', () => {
+    renderMainNav()
+    //screen.debug() // uncomment to see the rendered HTML
+    const companyName = screen.getByText('Bobo Careers')
+    expect(companyName).toBeInTheDocument()
+  })
+  it('displays menu items fot navegations', () => {
+    renderMainNav()
+    const navegationMenuItems = screen.getAllByRole('listitem')
+    const navegationMenuItemsText = navegationMenuItems.map((item) => item.textContent)
+    expect(navegationMenuItemsText).toEqual([
+      'Teams',
+      'Location',
+      'Life at Bobo careers',
+      'How we hire',
+      'Students',
+      'Jobs'
+    ])
+  })
 
-            const loginButton = screen.getByRole("button",{
-                name: /sing in/i,
-            })
-            await userEvent.click(loginButton)
-            profileImage = screen.getByRole("img", {
-                name: /user profile image/i, // use a regex to match the alt text
-            })
-            expect(profileImage).toBeInTheDocument()
-        });
+  describe('When the user logs in', () => {
+    it('displays the profile picture', async () => {
+      renderMainNav()
+      let profileImage = screen.queryByRole('img', {
+        name: /user profile image/i // use a regex to match the alt text
+      })
+      expect(profileImage).not.toBeInTheDocument()
+
+      const loginButton = screen.getByRole('button', {
+        name: /sing in/i
+      })
+      await userEvent.click(loginButton)
+      profileImage = screen.getByRole('img', {
+        name: /user profile image/i // use a regex to match the alt text
+      })
+      expect(profileImage).toBeInTheDocument()
     })
+  })
 })
