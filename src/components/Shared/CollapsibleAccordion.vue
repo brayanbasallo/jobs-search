9ae6e70f849daec12f5990b631414d8649941ed3
basallo17 <basallo17@gmail.com>
2023-12-19 02:54:12 +0000

    feat: fallback content

--- src/components/Shared/CollapsibleAccordion.vue
+++ src/components/Shared/CollapsibleAccordion.vue
@@ -9,7 +9,9 @@
       <font-awesome-icon :icon="caretIcon"></font-awesome-icon>
     </div>
     <div v-if="isOpen" class="mt-5 w-full">
-        <slot></slot>
+        <slot>
+          <p>Opps! No content provided.</p>
+        </slot>
     </div>
   </div>
 </template>
