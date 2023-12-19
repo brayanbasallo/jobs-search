386bde2f69adac6f44dbf9b638405eece695126b
basallo17 <basallo17@gmail.com>
2023-12-19 03:20:30 +0000

    refactor: optimized and refactor jobFiltersSidebard

--- src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -10,30 +10,7 @@
       <collapsible-accordion header="Job types">
         <p>Placeholder for now</p>
       </collapsible-accordion>
-      <collapsible-accordion header="Orgniztions">
-        <div class="mt-5">
-          <fieldset>
-            <ul class="flex flex-row flex-wrap">
-              <li class="h-8 w-1/2">
-                <input id="VueTube" type="checkbox" class="mr-3" />
-                <label for="VueTube">VueTube</label>
-              </li>
-              <li class="h-8 w-1/2">
-                <input id="Between Vue and Me" type="checkbox" class="mr-3" />
-                <label for="Between Vue and Me">Between Vue</label>
-              </li>
-              <li class="h-8 w-1/2">
-                <input id="Et Vue Vrute" type="checkbox" class="mr-3" />
-                <label for="Et Vue Vrute">Et Vue Brute</label>
-              </li>
-              <li class="h-8 w-1/2">
-                <input id="Vue and a Half Men" type="checkbox" class="mr-3" />
-                <label for="Vue and a Half Men">Vue and a Half Men</label>
-              </li>
-            </ul>
-          </fieldset>
-        </div>
-      </collapsible-accordion>
+      <job-filters-sidebar-organizations />
     </section>
   </div>
 </template>
@@ -41,12 +18,14 @@
 <script>
 import ActionButton from '@/components/Shared/ActionButton.vue'
 import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
+import JobFiltersSidebarOrganizations from '@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue';
 
 export default {
   name: 'JobFiltersSidebar',
   components: {
     ActionButton,
-    CollapsibleAccordion
+    CollapsibleAccordion,
+    JobFiltersSidebarOrganizations
   }
 }
 </script>
