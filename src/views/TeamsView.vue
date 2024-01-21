959717e67107dcdd27802513bd17717bac9936a8
Brayan Basallo Soto <basallo17@gmail.com>
2024-01-21 17:58:00 -0500

    feat: add title teams view

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -1,5 +1,12 @@
 <template>
-  <HeaderContainer></HeaderContainer>
+  <HeaderContainer>
+    <template #title>
+      <h1 class="text-4xl font-normal w-full">Teams</h1>
+    </template>
+    <template #subtitle>
+      <h2 class="my-4 w-full text-base font-light">It's awesome work here. why don't you come joiun us?</h2>
+    </template>
+  </HeaderContainer>
 </template>
 
 <script>
