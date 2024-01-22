9f4d3ef81210a0f0e18ed4244db9cbb67cc2cc9e
Brayan Basallo Soto <basallo17@gmail.com>
2024-01-21 19:45:41 -0500

    test: testing spotLight

---
+++ tests/unit/components/JobSearch/SpotLight.test.js
@@ -0,0 +1,86 @@
+import { screen, render } from '@testing-library/vue'
+import axios from 'axios'
+
+import SpotLight from '@/components/JobSearch/SpotLight.vue'
+
+vi.mock('axios')
+
+describe('SpotLight.vue', () => {
+    it("provids image to parent component", async () => {
+        axios.get.mockResolvedValue({
+            data: [
+                {
+                    id: 1,
+                    img: "some image",
+                    title: "some title",
+                    description: "some description"
+                }
+            ]
+        })
+
+        render(SpotLight, {
+            slots: {
+                default: `
+                    <template #default="slotProps">
+                        <h1>{{ slotProps.img }}</h1>
+                    </template>
+                `
+            }
+        })
+
+        const text = await screen.findByText("some image")
+        expect(text).toBeInTheDocument()
+    })
+
+    it("provids title to parent component", async () => {
+        axios.get.mockResolvedValue({
+            data: [
+                {
+                    id: 1,
+                    img: "some image",
+                    title: "some title",
+                    description: "some description"
+                }
+            ]
+        })
+
+        render(SpotLight, {
+            slots: {
+                default: `
+                    <template #default="slotProps">
+                        <h1>{{ slotProps.title }}</h1>
+                    </template>
+                `
+            }
+        })
+
+        const title = await screen.findByText("some title")
+        expect(title).toBeInTheDocument()
+    })
+
+    it("provids description to parent component", async () => {
+        axios.get.mockResolvedValue({
+            data: [
+                {
+                    id: 1,
+                    img: "some image",
+                    title: "some title",
+                    description: "some description"
+                }
+            ]
+        })
+
+        render(SpotLight, {
+            slots: {
+                default: `
+                    <template #default="slotProps">
+                        <h1>{{ slotProps.description }}</h1>
+                    </template>
+                `
+            }
+        })
+
+        const description = await screen.findByText("some description")
+        expect(description).toBeInTheDocument()
+    })
+})
