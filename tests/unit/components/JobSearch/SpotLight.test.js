f2b7e63f0c43e494f633be30bc7e098a7925244c
Brayan Basallo Soto <basallo17@gmail.com>
2024-01-21 19:52:24 -0500

    test: refactor test

--- tests/unit/components/JobSearch/SpotLight.test.js
+++ tests/unit/components/JobSearch/SpotLight.test.js
@@ -6,81 +6,72 @@ import SpotLight from '@/components/JobSearch/SpotLight.vue'
 vi.mock('axios')
 
 describe('SpotLight.vue', () => {
-    it("provids image to parent component", async () => {
-        axios.get.mockResolvedValue({
-            data: [
-                {
-                    id: 1,
-                    img: "some image",
-                    title: "some title",
-                    description: "some description"
-                }
-            ]
-        })
+  const mockSpotLightsRender = (spotLight = []) => {
+    axios.get.mockResolvedValue({
+      data: [
+        {
+          id: 1,
+          img: 'some image',
+          title: 'some title',
+          description: 'some description',
+          ...spotLight
+        }
+      ]
+    })
+  }
 
-        render(SpotLight, {
-            slots: {
-                default: `
+  it('provids image to parent component', async () => {
+    mockSpotLightsRender()
+    render(SpotLight, {
+      slots: {
+        default: `
                     <template #default="slotProps">
                         <h1>{{ slotProps.img }}</h1>
                     </template>
                 `
-            }
-        })
-
-        const text = await screen.findByText("some image")
-        expect(text).toBeInTheDocument()
+      }
     })
 
-    it("provids title to parent component", async () => {
-        axios.get.mockResolvedValue({
-            data: [
-                {
-                    id: 1,
-                    img: "some image",
-                    title: "some title",
-                    description: "some description"
-                }
-            ]
-        })
+    const text = await screen.findByText('some image')
+    expect(text).toBeInTheDocument()
+  })
 
-        render(SpotLight, {
-            slots: {
-                default: `
+  it('provids title to parent component', async () => {
+    const spotLight = {
+      title: 'Another title'
+    }
+    mockSpotLightsRender(spotLight)
+
+    render(SpotLight, {
+      slots: {
+        default: `
                     <template #default="slotProps">
                         <h1>{{ slotProps.title }}</h1>
                     </template>
                 `
-            }
-        })
-
-        const title = await screen.findByText("some title")
-        expect(title).toBeInTheDocument()
+      }
     })
 
-    it("provids description to parent component", async () => {
-        axios.get.mockResolvedValue({
-            data: [
-                {
-                    id: 1,
-                    img: "some image",
-                    title: "some title",
-                    description: "some description"
-                }
-            ]
-        })
+    const title = await screen.findByText('Another title')
+    expect(title).toBeInTheDocument()
+  })
 
-        render(SpotLight, {
-            slots: {
-                default: `
+  it('provids description to parent component', async () => {
+    const spotLight = {
+      description: 'Another description'
+    }
+    mockSpotLightsRender(spotLight)
+    render(SpotLight, {
+      slots: {
+        default: `
                     <template #default="slotProps">
                         <h1>{{ slotProps.description }}</h1>
                     </template>
                 `
-            }
-        })
-
-        const description = await screen.findByText("some description")
-        expect(description).toBeInTheDocument()
+      }
     })
+
+    const description = await screen.findByText('Another description')
+    expect(description).toBeInTheDocument()
+  })
 })
