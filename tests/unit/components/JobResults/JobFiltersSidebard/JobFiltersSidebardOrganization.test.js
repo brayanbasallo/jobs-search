31b115893390e0852707a127eea3d98a7babf5d3
Brayan Basallo Soto <basallo17@gmail.com>
2024-02-08 00:37:03 -0500

    test: refactor test

--- tests/unit/components/JobResults/JobFiltersSidebard/JobFiltersSidebardOrganization.test.js
+++ tests/unit/components/JobResults/JobFiltersSidebard/JobFiltersSidebardOrganization.test.js
@@ -4,13 +4,13 @@ import { createTestingPinia } from '@pinia/testing'
 
 import JobFiltersSidebard from '@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue'
 import { useJobsStore } from '@/stores/jobs'
+import { useUserStore } from '@/stores/user'
 
 describe('JobFiltersSidebard.vue', () => {
-  it('renders unique list of organizations from jobs store', async () => {
+  const renderComponent = () => {
     const pinia = createTestingPinia()
     const jobsStore = useJobsStore()
-
-    jobsStore.UNIQUE_ORGANIZATIONS = new Set(['org 1', 'org 2', 'org 3', 'org 4', 'org 5'])
+    const userStore = useUserStore()
 
     render(JobFiltersSidebard, {
       global: {
@@ -21,11 +21,32 @@ describe('JobFiltersSidebard.vue', () => {
       }
     })
 
+    return { jobsStore, userStore }
+  }
+  it('renders unique list of organizations from jobs store', async () => {
+    const { jobsStore } = renderComponent()
+
+    jobsStore.UNIQUE_ORGANIZATIONS = new Set(['org 1', 'org 2', 'org 3', 'org 4', 'org 5'])
+
     const button = screen.getByRole('button', { name: /organizations/i })
     await userEvent.click(button)
 
     const organizationsListItems = screen.getAllByRole('listitem')
     const organizations = organizationsListItems.map((item) => item.textContent)
     expect(organizations).toEqual(Array.from(jobsStore.UNIQUE_ORGANIZATIONS))
+  })
+
+  it("communicates that user has selected checkbox for organization", async () => {
+    const { jobsStore, userStore } = renderComponent()
+
+    jobsStore.UNIQUE_ORGANIZATIONS = new Set(['org 1', 'org 2', 'org 3', 'org 4', 'org 5'])
+
+    const button = screen.getByRole('button', { name: /organizations/i })
+    await userEvent.click(button)
+
+    const org1Checkbox = screen.getByRole('checkbox', { name: /org 1/i })
+    await userEvent.click(org1Checkbox)
+
+    expect(userStore.ADD_SELECTED_ORGANIZATION).toHaveBeenCalledWith(['org 1'])
   })
 })
