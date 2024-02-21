1ead4f237f9a8be79c84018ebae0cb6f1859c394
Brayan Basallo Soto <basallo17@gmail.com>
2024-02-20 23:49:37 -0500

    feat: allow multiple filters

--- src/stores/jobs.js
+++ src/stores/jobs.js
@@ -5,6 +5,7 @@ import { useUserStore } from '@/stores/user'
 export const FETCH_JOBS = 'FETCH_JOBS'
 export const UNIQUE_ORGANIZATIONS = 'UNIQUE_ORGANIZATIONS'
 export const UNIQUE_JOB_TYPES = 'UNIQUE_JOB_TYPES'
+export const FILTERED_JOBS = 'FILTERED_JOBS'
 export const FILTER_JOBS_BY_ORGANIZATION = 'FILTER_JOBS_BY_ORGANIZATION'
 export const FILTER_JOBS_BY_BY_JOB_TYPES = 'FILTER_JOBS_BY_BY_JOB_TYPES'
 
@@ -41,6 +42,21 @@ export const useJobsStore = defineStore('jobs', {
         return state.jobs
       
       return state.jobs.filter((job) => userStore.selectedJobTypes.includes(job.jobType))
+    },
+    [FILTERED_JOBS](state) {
+      const userStore = useUserStore()
+
+      const noSelectedOrganizations = userStore.selectedOrganizations.length === 0
+      const noSelectedJobTypes = userStore.selectedJobTypes.length === 0
+     
+
+      return state.jobs.filter((job) => {
+        if (noSelectedOrganizations) return true
+        return userStore.selectedOrganizations.includes(job.organization)
+      }).filter((job) => {
+        if (noSelectedJobTypes) return true
+        return userStore.selectedJobTypes.includes(job.jobType)
+      })
     }
   }
 })
